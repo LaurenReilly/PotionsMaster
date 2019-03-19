6b2846531199b2ed8e4a270102c1f444af1b1bae
Lauren Reilly <LaurenReillyNet@gmail.com>
2019-03-19 19:51:05 -0400

    removed /build from gitignore

---
+++ game/build/precache-manifest.6ec93d994e8260c1cdedfc48ba4134ad.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = [
+  {
+    "revision": "be7d0563719edee9a870b5eda1f3c3d7",
+    "url": "/static/media/beetle.be7d0563.png"
+  },
+  {
+    "revision": "348ab7273bf09f5b3464",
+    "url": "/static/css/main.54a4c18c.chunk.css"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "343425cd9098472ee036",
+    "url": "/static/js/2.343425cd.chunk.js"
+  },
+  {
+    "revision": "cd2eeeeef11c47fb1653f5001822f251",
+    "url": "/static/media/snape.cd2eeeee.png"
+  },
+  {
+    "revision": "e495b408a018d3d4442892791cb155d5",
+    "url": "/static/media/cauldron.e495b408.png"
+  },
+  {
+    "revision": "8ba5bdb92e03b117bcff2abb647dd4b4",
+    "url": "/static/media/lacewing.8ba5bdb9.png"
+  },
+  {
+    "revision": "2ee5c9f727ee173f126c2d4be5c80d22",
+    "url": "/static/media/boomslang.2ee5c9f7.png"
+  },
+  {
+    "revision": "1eeb3418a747831d5d58b9c85a09cc3e",
+    "url": "/static/media/bicorn.1eeb3418.png"
+  },
+  {
+    "revision": "498f13f925df447381f9547c51704d5e",
+    "url": "/static/media/dragon.498f13f9.png"
+  },
+  {
+    "revision": "348ab7273bf09f5b3464",
+    "url": "/static/js/main.348ab727.chunk.js"
+  },
+  {
+    "revision": "bb324efa6172b42f795506fc85805b6c",
+    "url": "/static/media/newt.bb324efa.png"
+  },
+  {
+    "revision": "e41d1af78ef0023c36a80dcf40771e14",
+    "url": "/static/media/crinkle.e41d1af7.mp3"
+  },
+  {
+    "revision": "3cd64737b2f233488a61a71f2bc8face",
+    "url": "/static/media/drip.3cd64737.wav"
+  },
+  {
+    "revision": "67c4c069f689fcefdbc0ce4e43115149",
+    "url": "/static/media/drop.67c4c069.wav"
+  },
+  {
+    "revision": "78f6a085d5167a3131bf0ee333c7ce0d",
+    "url": "/static/media/dust.78f6a085.wav"
+  },
+  {
+    "revision": "8da90b05c0e875bfc4c5f0bb66e0d7cf",
+    "url": "/static/media/seeds.8da90b05.wav"
+  },
+  {
+    "revision": "37ec5ff6f28e3b4342a269b181ee8c68",
+    "url": "/static/media/sizzle.37ec5ff6.mp3"
+  },
+  {
+    "revision": "bdd8f5f283576caef3635f57ea2cf51f",
+    "url": "/static/media/bubbling.bdd8f5f2.wav"
+  },
+  {
+    "revision": "1ff1daade674d44c3a14a8e38c5d264c",
+    "url": "/static/media/themeSong.1ff1daad.mp3"
+  },
+  {
+    "revision": "7c74a84079deddb71d60ec4c1be90801",
+    "url": "/index.html"
+  }
+];
