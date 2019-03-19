6846f6f0e02ee9e3210dc598632d98f7d5022e8f
Lauren Reilly <LaurenReillyNet@gmail.com>
2019-03-19 19:31:58 -0400

    added error message to return json err

--- app.js
+++ app.js
@@ -53,7 +53,8 @@ app.use(function(err, req, res, next) {
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   res.status(err.status || 500);
-  res.send('error')
+  // res.send('error')
+  res.json(err)
 });
 
 module.exports = app;
