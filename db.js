e79fd0fffd99556fab8213ff6776511af90bfd2c
Lauren Reilly <LaurenReillyNet@gmail.com>
2019-05-02 13:33:26 -0400

    added database URI from heroku

--- db.js
+++ db.js
@@ -1,14 +1,14 @@
 if(process.env.NODE_ENV !== 'production') require('dotenv').config();
+
+// Loading and initializing the library:
 const pgp = require('pg-promise')({});
 
-const cn = {
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    database: process.env.DATABASE,
-    user: process.env.USERNAME,
-    password: process.env.PASSWORD
-};
+// Preparing the connection details:
+const cn = 'postgres://ydiyucsirgjohz:25f3dcae2adf10bc838d93fe2bc9c4dbac9f59a5850156313c3d6f9f3290a6fd@ec2-54-235-114-242.compute-1.amazonaws.com:5432/daac4nui56rl1f?ssl=true';
 
-const db = pgp(cn); // database instance;
 
+// Creating a new database instance from the connection details:
+const db = pgp(cn);
+
+// Exporting the database object for shared use:
 module.exports = db;
